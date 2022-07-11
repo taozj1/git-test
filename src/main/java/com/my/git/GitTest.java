59c0cdb0f606fc05bd9de83d1469d0a351886d2e
taozj <1750277503@qq.com>
2022-07-11 14:45:00 +0800

    hotfix

--- src/main/java/com/my/git/GitTest.java
+++ src/main/java/com/my/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("pull test1");
         System.out.println("pull test2");
         System.out.println("pull test3");
+        System.out.println("pull test4");
     }
 }
